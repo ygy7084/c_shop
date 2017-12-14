415be4ddd489dd758f686a30cd41f782adb5fed0
DESKTOP-F9P49HO\1nept <1neptune1@naver.com>
2017-12-14 11:56:49 +0900

    clean

--- src/scenes/Point/data/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { reducer as savePointReducer } from './savePoint/reducer';
-
-export const reducer = combineReducers({
-  savePoint: savePointReducer,
-});
